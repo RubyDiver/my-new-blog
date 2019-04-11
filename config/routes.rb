6e6b253e1248cc3b873810f0d8dd74f0c829dcab
Paweł <tatarynowiczpawel2@gmail.com>
2019-04-12 00:27:36 +0200

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+
   resources :articles
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  # resources :users, except: [:new]
 end
